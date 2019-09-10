de8b8747738b1ce6d5b153347ce91916c1847060
tbag <692603114@qq.com>
2019-09-10 08:49:57 +0800

    注释

--- src/commlib/co/trans_mgr.h
+++ src/commlib/co/trans_mgr.h
@@ -12,6 +12,13 @@
 
 class TransMgr : public S<TransMgr> {
 public:
+    /**
+     * 注册事务类T
+     * @tparam T 注册的类
+     * @param cmd 注册的key
+     * @param count 注册T事务 的数量
+     * @return
+     */
     template <class T>
     bool RegisterCmd(uint32_t cmd, int count) {
         if (trans_map_.find(cmd) != trans_map_.end()) {
@@ -23,8 +30,14 @@ public:
         return true;
     }
 
+    /**
+     * 唤醒co_id对应的事务。如果co_id == 0则为cmd创建一个事务
+     * @param cmd
+     * @param co_id
+     * @return
+     */
     int OnCmd(uint32_t cmd, uint32_t co_id) {
-        if (co_id == 0) {
+        if (co_id == 0) { // init
             auto &trans_pool = trans_map_[cmd];
             if (trans_pool.empty()) {
                 return -1;
@@ -32,6 +45,9 @@ public:
             auto trans = trans_pool.get_shared();
             auto id = co_task_.AddTack([trans](const CoYield &co) { trans->DoTask(co); });
             return co_task_.ResumeOne(id);
+        }
+        if (!co_task_.CoIdExist(co_id)) {
+            return -1;
         }
         return co_task_.ResumeOne(co_id);
     }
