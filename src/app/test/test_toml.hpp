c416b3ead8abde000e3a07a34ac7944be6699842
tbag <692603114@qq.com>
2020-03-17 09:41:53 +0800

    update config

--- src/app/test/test_toml.hpp
+++ src/app/test/test_toml.hpp
@@ -1,5 +1,32 @@
+/*********************************************************
+*
+* 文件自动生成. tool: https://github.com/liuping001/toml_cpp
+*
+**********************************************************/
+
 #pragma once
-#include "commlib/toml_base.hpp"
+#include "third/cpptoml/cpptoml.h"
+
+
+#ifndef TOML_BASE_STRUCT
+#define TOML_BASE_STRUCT
+struct TomlBase {
+  TomlBase(){}
+  TomlBase(std::shared_ptr<cpptoml::base> ptr) : ptr_(ptr) {}
+  std::string operator ()() const { return ptr_->as<std::string>()->get(); }
+  int64_t I() const { return ptr_->as<int64_t>()->get(); }
+  double D() const { return ptr_->as<double>()->get(); }
+  bool B() const { return ptr_->as<bool>()->get(); }
+#if defined(TOML_DATE)
+  cpptoml::local_date LocalDate() const { return ptr_->as<cpptoml::local_date>()->get(); }
+  cpptoml::local_time LocalTime() const { return ptr_->as<cpptoml::local_time>()->get(); }
+  cpptoml::local_datetime LocalDatetime() const { return ptr_->as<cpptoml::local_datetime>()->get(); }
+  cpptoml::offset_datetime OffsetDatetime() const { return ptr_->as<cpptoml::offset_datetime>()->get(); }
+#endif
+ private:
+  std::shared_ptr<cpptoml::base> ptr_;
+};
+#endif
 
 namespace test_toml {
 
